72a98b0ad029295cf68873b263eff2405472b67b
Alexandros De Great <126134422+pichtranst123@users.noreply.github.com>
2024-05-10 13:30:26 +0700

    chore: adding account + admin + competition + survey controller

---
+++ eco-edu-mvc/Controllers/competitionsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eco_edu_mvc.Controllers;
+public class competitionsController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
