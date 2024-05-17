0c9fbf944c5b9151d7b15e4d9549bb1fe0fe58de
pichtran2004 <126134422+pichtranst123@users.noreply.github.com>
2024-05-17 13:27:17 +0700

    chore: update survey

---
+++ eco-edu-mvc/Controllers/SurveysController.cs
@@ -0,0 +1,115 @@
+using eco_edu_mvc.Models.Entities;
+using eco_edu_mvc.Models.SurveyModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eco_edu_mvc.Controllers;
+public class SurveysController(EcoEduContext context) : Controller
+{
+	private readonly EcoEduContext _context = context;
+
+	[HttpGet]
+	public async Task<IActionResult> List() => View(await _context.Surveys.ToListAsync());
+
+	[HttpGet]
+	public async Task<IActionResult> Details(int id)
+	{
+		var survey = await _context.Surveys.FirstOrDefaultAsync(p => p.SurveyId == id);
+		if (survey == null) return NotFound(); 
+
+		return View(survey);
+	}
+
+	public ActionResult Post() => View();
+
+	[HttpPost]
+	public async Task<IActionResult> Post(SurveyModel model)
+	{
+		if (!ModelState.IsValid) return View(model);
+
+		Survey survey = new()
+		{
+			Title = model.Title,
+			Topic = model.Topic,
+			CreatedBy = model.CreatedBy,
+			CreateDate = DateTime.UtcNow,
+			EndDate = model.EndDate,
+			TargetAudience = model.TargetAudience,
+			Active = model.Active
+		};
+		_context.Surveys.Add(survey);
+		await _context.SaveChangesAsync();
+
+		return CreatedAtAction(nameof(List), new { id = survey.SurveyId }, survey);
+	}
+
+	public async Task<IActionResult> Update(int id)
+	{
+		var survey = await _context.Surveys.FindAsync(id);
+		if (survey == null) return NotFound();
+
+		SurveyModel model = new()
+		{
+			SurveyId = survey.SurveyId,
+			Title = survey.Title,
+			Topic = survey.Topic,
+			CreatedBy = survey.CreatedBy,
+			CreateDate = survey.CreateDate,
+			EndDate = survey.EndDate,
+			TargetAudience = survey.TargetAudience,
+			Active = survey.Active
+		};
+		return View(model);
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> Update(int id, SurveyModel model)
+	{
+		if (id != model.SurveyId) return NotFound();
+
+		if (!ModelState.IsValid) return View(model);
+
+		var survey = await _context.Surveys.FindAsync(id);
+		if (survey == null) return NotFound();
+
+		try
+		{
+			survey.Title = model.Title;
+			survey.Topic = model.Topic;
+			survey.CreateDate = model.CreateDate;
+			survey.EndDate = model.EndDate;
+			survey.TargetAudience = model.TargetAudience;
+			survey.Active = model.Active;
+
+			_context.Surveys.Update(survey);
+			await _context.SaveChangesAsync();
+
+			return RedirectToAction(nameof(List));
+		}
+		catch (Exception ex)
+		{
+			ModelState.AddModelError("Error", ex.Message);
+		}
+		return View(model);
+	}
+
+	public async Task<IActionResult> Delete(int id)
+	{
+		var survey = await _context.Surveys.FindAsync(id);
+		if (survey == null) return NotFound();
+
+		return View(survey);
+	}
+
+	[HttpPost, ActionName("Delete")]
+	public async Task<IActionResult> DeleteConfirm(int id)
+	{
+		var survey = await _context.Surveys.FindAsync(id);
+		if (survey == null) return NotFound();
+
+		_context.Surveys.Remove(survey);
+		await _context.SaveChangesAsync();
+
+		return RedirectToAction(nameof(List));
+	}
+}
