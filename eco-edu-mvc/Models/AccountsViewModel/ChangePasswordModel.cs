ca9a6b3fb542f40161c7c8ea47378b842bf4b8dc
Hiếu Minh <129259600+HieuMinh1309@users.noreply.github.com>
2024-05-20 14:07:45 +0700

    Chore: optimize view and account function

--- eco-edu-mvc/Models/AccountsViewModel/ChangePasswordModel.cs
+++ eco-edu-mvc/Models/AccountsViewModel/ChangePasswordModel.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace eco_edu_mvc.Models.AccountsViewModel;
 
 public class ChangePasswordModel
 {
-    [Required, DataType(DataType.Password)]
-    public string Password { get; set; }
-	[Required, DataType(DataType.Password),
+    public string Username { get; set; }
+
+	[Required, DataType(DataType.Password)]
+	public string Password { get; set; }
+
+	[Required, DataType(DataType.Password), 
 				Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
 	public string ConfirmPassword { get; set; }
 }
---
+++ eco-edu-mvc/Models/AccountsViewModel/CheckOnlyCode.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eco_edu_mvc.Models.AccountsViewModel;
+
+public class CheckOnlyCode
+{
+    [Required]
+    public string code { get; set; }
+}
