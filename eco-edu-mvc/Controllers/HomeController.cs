8c152aa37a323efbdc0ea122fa460e4f749799ae
Hiếu Minh <129259600+HieuMinh1309@users.noreply.github.com>
2024-06-10 10:18:48 +0700

    chore: fix bug and optimize

--- eco-edu-mvc/Controllers/HomeController.cs
+++ eco-edu-mvc/Controllers/HomeController.cs
@@ -14,18 +14,18 @@ public class HomeController(EcoEduContext context) : Controller
     {
         var surveys = await _context.Surveys.Where(s => s.Active == true).OrderByDescending(s => s.CreateDate).Take(4).ToListAsync();
         var competitions = await _context.Competitions.Where(c => c.Active == true).OrderByDescending(s => s.StartDate).Take(6).ToListAsync();
-        var winners = await _context.GradeTests.Include(g => g.Entry).ThenInclude(e => e.User).OrderByDescending(w => w.Score).ToListAsync();
+        //var winners = await _context.GradeTests.Include(g => g.Entry).ThenInclude(e => e.User).OrderByDescending(w => w.Score).ToListAsync();
 
         // I have to seperate these so the program wont go wrong.
-        var topWinner = winners.FirstOrDefault();
-        var nextWinners = winners.Skip(1).Take(3).ToList();
+        //var topWinner = winners.FirstOrDefault();
+        //var nextWinners = winners.Skip(1).Take(3).ToList();
 
         HomeModel model = new()
         {
             Surveys = surveys,
             Competitions = competitions,
-            TopWinner = topWinner,
-            NextWinners = nextWinners
+            //TopWinner = topWinner,
+            //NextWinners = nextWinners
         };
         return View(model);
     }
