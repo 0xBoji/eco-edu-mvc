f5665df9c985f937e1e7e4351997e067624a64e2
ThaiNguyen200104 <quangthai0997@gmail.com>
2024-06-10 13:25:34 +0700

    chore

--- eco-edu-mvc/Models/Entities/SeminarMember.cs
+++ eco-edu-mvc/Models/Entities/SeminarMember.cs
@@ -13,8 +13,6 @@ public partial class SeminarMember
 
     public virtual ICollection<Seminar> Seminars { get; set; } = new List<Seminar>();
 
+    public virtual Seminar Seminar {  get; set; }
     public virtual User User { get; set; } = null!;
-
-    public virtual Seminar Seminar { get; set; } = null!;
-
 }
